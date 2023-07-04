0bfdd182e628efcd1514392b3928dd932faa35f1
yahyafullstack01 <yahyafullstack01@gmail.com>
2023-07-04 22:46:31 +0200

    create schema and send the first

--- server.js
+++ server.js
@@ -14,6 +14,52 @@ mongoose.connect("mongodb+srv://yahyafullstack01:Yahya1998@yahya.nmc63m5.mongodb
 .then((()=> console.log("connected to mongodb")))
 .catch(error=>console.error("failed to connect to mongo db",error));
 
+const UserSchema = new mongoose.Schema({
+    username: {type: String, required: true},
+    useremail:{ type :String,required:true },
+    package: {type:String},
+    userphone:{type:String, required:true},
+    sessionsnumber:{type:String},
+    reservationDate:[{
+        date:{type:String, required:true},
+        time:{type:String, required:true},
+    }]
+});
+
+UserSchema.pre('save', function(next){
+    this.reservationDate= this.reservationDate.map((item)=>({
+        date : new Date(item.date).toISOString().split('T')[0],
+        time : item.time,
+
+    })
+    )
+    next();
+})
+
+const Users=new mongoose.model('Users',UserSchema);
+
+const test = new Users({
+    username: 'Harry',
+    useremail: 'Harry@gmail.com',
+    userphone: '1231232',
+    package:'',
+    sessionsnumber:'12',
+    
+    reservationDate: [
+        { date: new Date('2023-07-12'), time: '09:00 AM' },
+        { date: new Date('2023-07-12'), time: '10:00 AM' },
+        { date: new Date('2023-07-12'), time: '11:00 AM' },
+    ]
+});
+
+test.save()
+    .then(() => {
+        console.log('User saved successfully');
+    })
+    .catch((error) => {
+        console.error('Error saving user:', error);
+    });
+
 
 
 
