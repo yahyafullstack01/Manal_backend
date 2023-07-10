206779b840bf50439a9afe2d42849a77cd953ef2
Grimhunter <pandatech1999@gmail.com>
2023-07-10 11:37:29 +0200

    Sending Date and Time to the Db

--- server.js
+++ server.js
@@ -15,97 +15,73 @@ mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
     .then((() => console.log("connected to mongodb")))
     .catch(error => console.error("failed to connect to mongo db", error));
 
-
 //schema creation to send the date to mongo
 const UserSchema = new mongoose.Schema({
     username: { type: String, required: true },
-    useremail: { type: String, required: true },
+    useremail: { type: String, required: true, lowercase: true },
     userPhone: { type: String, required: true },
     user_session_number: { type: String },
     packageType: { type: String },
-    // reservationDate: [{
-    //     _id: false,
-    //     date: { type: String, required: true },
-    //     time: { type: String, required: true },
-    // }]
+    reservationDate: [
+        {
+            _id: false,
+            date: { type: String, required: true },
+            time: { type: String, required: true },
+        },
+    ],
 });
 
-// UserSchema.pre('save', function (next) {
-//     this.reservationDate = this.reservationDate.map((item) => ({
-//         date: new Date(item.date).toISOString().split('T')[0],
-//         time: item.time,
+UserSchema.pre('save', function (next) {
+    this.reservationDate = this.reservationDate.map((item) => ({
+        date: new Date(item.date).toISOString().split('T')[0],
+        time: item.time,
+    }));
+    next();
+});
 
-//     })
-//     )
-//     next();
-// })
-
-const Users = new mongoose.model('Users', UserSchema);
+// Create a Mongoose model based on the user schema
+const User = mongoose.model('Users', UserSchema);
 
 app.post('/api/bookings', (req, res) => {
-    const { username, useremail, user_session_number, userPhone, packageType } = req.body;
+    const { username, useremail, user_session_number, userPhone, packageType, reservationDate } = req.body;
 
-    // Create a new User instance with the form data
-    const newUser = new Users({
+    const newUser = new User({
         username,
         useremail,
         userPhone,
         user_session_number,
-        packageType
+        packageType,
+        reservationDate,
     });
 
-    // Save the user to the database
-    newUser.save()
+    newUser
+        .save()
         .then(() => {
             res.json({ message: 'Booking created successfully' });
         })
-        .catch(error => {
+        .catch((error) => {
             console.error('Error saving booking:', error);
             res.status(500).json({ message: 'Failed to create booking' });
         });
 });
 
-//geting  data from  mongoDb
-// app.get('/api/users', async (req, res) => {
-//     try {
-//         const data = await Users.find({});
-//         const dateTimeArr = data.map((item) => item.reservationDate).flat();
-//         const FilteredDateTime = dateTimeArr.map((obj) => {
-//             const newObj = { ...obj._doc };
-//             return newObj;
-//         });
-//         res.json(FilteredDateTime);
-//     } catch (error) {
-//         console.error('Error fetching data:', error);
-//         res.status(500).json({ message: 'Failed to fetch data' });
-//     }
-// });
 
-
-
-//Example for sending  data to mongodb
-// const test = new Users({
-//     username: 'Sss',
-//     useremail: 'Samer@gmail.com',
-//     userphone: '1231232',
-//     package: '',
-//     sessionsnumber: '12',
-//     reservationDate: [
-//         { date: new Date('2023-07-12'), time: '09:00 AM' },
-//         { date: new Date('2023-07-12'), time: '10:00 AM' },
-//         { date: new Date('2023-07-12'), time: '11:00 AM' },
-//     ]
-// });
-
-// test.save()
-//     .then(() => {
-//         console.log('User saved successfully');
-//     })
-//     .catch((error) => {
-//         console.error('Error saving user:', error);
-//     });
+app.get('/api/users', async (req, res) => {
+    try {
+        const data = await User.find({});
+        const dateTimeArr = data.map((item) => item.reservationDate).flat();
+        const filteredDateTime = dateTimeArr.map((obj) => {
+            const newObj = { ...obj._doc };
+            return newObj;
+        });
+        res.json(filteredDateTime);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        res.status(500).json({ message: 'Failed to fetch data' });
+    }
+});
 
 
 app.listen(port, () => {
-    console.log(`listening on ${port}`);
-})
+    console.log(`Server is running on port ${port}`);
+});
